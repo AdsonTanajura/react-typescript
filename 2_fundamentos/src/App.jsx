a13be1ce59f6c9cbe590632e608ea6212224aec1
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-24 19:47:43 -0300

    add all ex on app

--- 2_fundamentos/src/App.jsx
+++ 2_fundamentos/src/App.jsx
@@ -4,13 +4,16 @@ import BotaoAzul from './components/BotaoAzul'
 import BotaoEstilizado from './components/BotaoEstilizado'
 import Cachorro from './components/Cachorro'
 import Counter from './components/Counter'
+import CounterEx from './components/CounterEx'
 import Descricao from './components/Descricao'
 import Form from './components/Form'
+import Greeting from './components/Greeting'
 import LoginButton from './components/LoginButton'
 import NumberList from './components/NumberList'
 import Pai from './components/Pai'
 import PaiFuncao from './components/PaiFuncao'
 import RenderCondicional from './components/RenderCondicional'
+import TaskList from './components/TaskList'
 import UserInfoForm from './components/UserInfoForm'
 import Warning from './components/Warning'
 import Welcome from './components/Welcome'
@@ -35,6 +38,17 @@ const App = () => {
     <NumberList numbers={[1,2,3,4,5,6,7]}/>
     <BotaoEstilizado />
     <BotaoAzul />
+    {/* Execericios */}
+    <Greeting name={'Adson'} /> 
+    <CounterEx />
+    <TaskList tasks={[
+  { id: 1, task: "Estudar JavaScript" },
+  { id: 2, task: "Criar layout em Tailwind CSS" },
+  { id: 3, task: "Configurar servidor com Node.js" },
+  { id: 4, task: "Revisar código do projeto Kanban" },
+  { id: 5, task: "Desenvolver novo recurso no jogo da velha" }
+]}/> 
+    <TaskList />
     </>
   )
 }
