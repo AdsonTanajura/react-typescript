6dc771d80b6d8938310e41ca46fe07973b7cf1d3
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-24 19:00:57 -0300

    Expressoes rernarias e listas

--- 2_fundamentos/src/App.jsx
+++ 2_fundamentos/src/App.jsx
@@ -5,10 +5,12 @@ import Counter from './components/Counter'
 import Descricao from './components/Descricao'
 import Form from './components/Form'
 import LoginButton from './components/LoginButton'
+import NumberList from './components/NumberList'
 import Pai from './components/Pai'
 import PaiFuncao from './components/PaiFuncao'
 import RenderCondicional from './components/RenderCondicional'
 import UserInfoForm from './components/UserInfoForm'
+import Warning from './components/Warning'
 import Welcome from './components/Welcome'
 
 const App = () => {
@@ -27,6 +29,8 @@ const App = () => {
     <RenderCondicional user={'Adson'}/>
     <LoginButton loggedIn={true}/>
     <LoginButton loggedIn={false}/>
+    <Warning warning={true}/>
+    <NumberList numbers={[1,2,3,4,5,6,7]}/>
     </>
   )
 }
