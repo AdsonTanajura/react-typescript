56f858b54c86f19aca8a53a70e8ad64dc57b4323
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-24 20:18:30 -0300

    useEffect

---
+++ 3_hooks/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ExemploUseEffect from './components/ExemploUseEffect/ExemploUseEffect'
+import Timer from './components/Timer/Timer'
+
+function App() {
+
+  return (
+    <>
+      <ExemploUseEffect />
+      <Timer />
+    </>
+  )
+}
+
+export default App
---
+++ 3_hooks/src/components/ExemploUseEffect/ExemploUseEffect.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react"
+
+const ExemploUseEffect = () => {
+    const [contador, setContador] = useState(0);
+    useEffect(() => {
+        document.title = `Você clicou ${contador} vezes`
+    }, [contador]);
+  return (
+    <div>
+      <p>Você clicou {contador} vezes</p>
+      <button onClick={() => setContador(contador + 1)}>Clique aqui</button>
+    </div>
+  )
+}
+
+export default ExemploUseEffect
