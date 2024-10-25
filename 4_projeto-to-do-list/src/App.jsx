b247fb40907e9e55ce8e62357678fd672d9d1248
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-25 20:12:19 -0300

    End Project e adiciona dados na localStorage

--- 4_projeto-to-do-list/src/App.jsx
+++ 4_projeto-to-do-list/src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TaskInput from "./components/TaskInput/TaskInput";
 import TaskList from "./components/TaskList/TaskList";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasks')) || []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
 
   const addTask = (task) => {
     setTasks([...tasks, { id: Date.now(), text: task, done: false }]);
@@ -13,13 +17,21 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== taskId))
   };
 
+  const toggleTaskDone = (taskId) => {
+    setTasks(tasks.map((task) => 
+      task.id === taskId ? {...task, done: !task.done} : task
+    ))
+  };
+
   return (
     <>
       <h1>Lista de Tarefas</h1>
       <TaskInput onAddTask={addTask} />
-      <TaskList tasks={tasks} onDeleteTask={deleteTask}/>
+      <TaskList tasks={tasks} onDeleteTask={deleteTask} onToggleTaskDone={toggleTaskDone}/>
     </>
   );
 }
 
 export default App;
+
+
--- 4_projeto-to-do-list/src/components/TaskList/TaskList.jsx
+++ 4_projeto-to-do-list/src/components/TaskList/TaskList.jsx
@@ -1,10 +1,10 @@
 import Task from "../Task/Task";
 
-const TaskList = ({ tasks, onDeleteTask }) => { // Corrigido de 'task' para 'tasks'
+const TaskList = ({ tasks, onDeleteTask, onToggleTaskDone }) => { // Corrigido de 'task' para 'tasks'
   return (
     <ul>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={() => onDeleteTask(task.id)} />
+        <Task key={task.id} task={task} onDelete={() => onDeleteTask(task.id)} onToggleDone={() => onToggleTaskDone(task.id)} />
       ))}
     </ul>
   );
