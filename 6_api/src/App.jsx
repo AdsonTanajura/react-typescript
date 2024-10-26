be6df2cd6bf8f13aa7c94fd4a0a6a7677228e5b3
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-26 00:39:01 -0300

    Custom Hook com requisicao para API

--- 6_api/src/App.jsx
+++ 6_api/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, BrowserRouter, Link } from 'react-router-dom'
 import FetchPosts from './components/FetchPosts'
 import AxiosPosts from './components/AxiosPosts'
 import PostManager from './components/PostManager'
+import PostLoader from './components/PostLoader'
 
 function App() {
 
@@ -17,10 +18,15 @@ function App() {
       <div>
         <Link to={'/posts'}>Gerenciar posts</Link>
       </div>
+      <div>
+        <Link to={'/posts/1'}>Carregar Post 1</Link>
+        <Link to={'/posts/999'}>Carregar Post 999</Link>
+      </div>
       <Routes>
         <Route path='/fetch-post' element={<FetchPosts />} />
         <Route path='/axios-post' element={<AxiosPosts />} />
         <Route path='/posts' element={<PostManager />}/>
+        <Route path='/posts/:postId' element={<PostLoader />}/>
       </Routes>
     </BrowserRouter>
   )
