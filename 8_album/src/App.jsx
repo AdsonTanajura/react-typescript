a1d0e4f04d2ea6bf9a711999d5b62a2dba721b16
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-28 19:43:52 -0300

    Configurando a chave da API

--- 8_album/src/App.jsx
+++ 8_album/src/App.jsx
@@ -1,18 +1,35 @@
 import { useEffect, useState } from 'react';
+import axio from 'axios';
 
-import FotoApliada from './components/FotoApliada/FotoApliada'
-import FotoList from './components/FotoList/FotoList'
-import SearchBar from './components/SearchBar/SearchBar'
+import FotoApliada from './components/FotoApliada/FotoApliada';
+import FotoList from './components/FotoList/FotoList';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [categoria, setCategoria] = useState('');
 
+  const fetchData = async ({ query, categoria }) => {
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
+    const response = await axio.get('https://api.unsplash.com/photos/random', {
+      params: {
+        client_id: apiKey,
+      },
+    });
+
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchData(query, categoria);
+  }, []);
   return (
-    <div className='container'>
+    <div className="container">
       <SearchBar />
       <FotoList />
       <FotoApliada />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
