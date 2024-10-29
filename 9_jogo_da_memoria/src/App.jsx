20a24ebef27399ca0f9d2ab0f6dbc3bc3f9bfc5a
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-28 21:05:06 -0300

    Setup inicial

---
+++ 9_jogo_da_memoria/src/App.jsx
@@ -0,0 +1,12 @@
+import Game from './components/Game/Game';
+
+function App() {
+  return (
+    <>
+      <h1>Jogo da Memoria</h1>
+      <Game />
+    </>
+  );
+}
+
+export default App;
---
+++ 9_jogo_da_memoria/src/components/Board/Board.jsx
@@ -0,0 +1,11 @@
+import Card from '../Card/Card';
+
+const Board = () => {
+  return (
+    <div className="bord">
+      <Card />
+    </div>
+  );
+};
+
+export default Board;
