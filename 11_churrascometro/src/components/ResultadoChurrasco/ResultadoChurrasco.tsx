2be1831f16c79406f1321f2a1027cb7a1f4959cb
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-29 22:00:48 -0300

    Cria a pagina de resultado, cria o component e exibe já com css

--- 11_churrascometro/src/components/ResultadoChurrasco/ResultadoChurrasco.tsx
+++ 11_churrascometro/src/components/ResultadoChurrasco/ResultadoChurrasco.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Alimento, nomesAlimentos, quantidadesPorPessoa } from '../../types';
+
+import styles from './ResultadoChurrasco.module.css';
+
+type ResultadosChurrasco = {
+  pessoas: number;
+  alimentosSelecionados: Alimento[];
+};
 
 const ResultadoChurrasco = () => {
-  return <div></div>;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const state = location.state as ResultadosChurrasco;
+
+  const totalPorAlimento = state.alimentosSelecionados.reduce(
+    (acc, alimento) => {
+      acc[alimento] = (quantidadesPorPessoa[alimento] * state.pessoas) / 1000; // Convertendo para kg
+      return acc;
+    },
+    {} as Record<Alimento, number>
+  );
+
+  const reiniciar = () => {
+    navigate('/');
+  };
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Resultado para {state.pessoas} pessoas:</h2>
+      {state.alimentosSelecionados.map((alimento) => (
+        <p key={alimento} className={styles.resultText}>
+          {nomesAlimentos[alimento]}: {totalPorAlimento[alimento]} kg
+        </p>
+      ))}
+      <button onClick={reiniciar} className={styles.resetButton}>
+        Reiniciar
+      </button>
+    </div>
+  );
 };
 
 export default ResultadoChurrasco;
--- 11_churrascometro/src/pages/Resultado/Resultado.tsx
+++ 11_churrascometro/src/pages/Resultado/Resultado.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-
+import bgImage from '../../assets/bg-2.jpg';
+import ResultadoChurrasco from '../../components/ResultadoChurrasco/ResultadoChurrasco';
 const Resultado = () => {
-  return <div></div>;
+  return (
+    <div
+      className="page-container"
+      style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover' }}
+    >
+      <ResultadoChurrasco />
+    </div>
+  );
 };
 
 export default Resultado;
