61ec243bdf5e1fb516d4c3b5e8fce2bec76342d6
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-30 15:31:06 -0300

    Resgatando usuario da API

--- 12_github_finder/src/components/Search.tsx/Search.tsx
+++ 12_github_finder/src/components/Search.tsx/Search.tsx
@@ -1,12 +1,22 @@
 import { BsSearch } from 'react-icons/bs';
-const Search = () => {
+import { useState } from 'react';
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+const Search = ({ loadUser }: SearchProps) => {
+  const [userName, setUserName] = useState('');
+
   return (
     <div>
       <h2>Busque por um usuário</h2>
       <p>Conheca seus melhores respositorios</p>
       <div>
-        <input type="text" placeholder="Digite o nome do usuario " />
-        <button>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuario "
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={() => loadUser(userName)}>
           <BsSearch />
         </button>
       </div>
--- 12_github_finder/src/routes/Home/Home.tsx
+++ 12_github_finder/src/routes/Home/Home.tsx
@@ -1,8 +1,18 @@
 import Search from '../../components/Search.tsx/Search';
+import { useState } from 'react';
+import { userProps } from '../../types/user';
 const Home = () => {
+  const [user, setUser] = useState<userProps | null>(null);
+
+  const loaderUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div>
-      <Search />
+      <Search loadUser={loaderUser} />
     </div>
   );
 };
