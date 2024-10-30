ccd38356efb3aa533e5062a873c39bf848cf2573
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-30 16:17:14 -0300

    Estilo do usuario

--- 12_github_finder/src/components/User/User.tsx
+++ 12_github_finder/src/components/User/User.tsx
@@ -1,6 +1,9 @@
 import { userProps } from '../../types/user';
 import { MdLocationPin } from 'react-icons/md';
+
 import { Link } from 'react-router-dom';
+
+import classes from './User.module.css';
 
 const User = ({
   avatar_url,
@@ -10,23 +13,23 @@ const User = ({
   login,
 }: userProps) => {
   return (
-    <div>
+    <div className={classes.user}>
       <img src={avatar_url} alt={login} />
       <h2>{login}</h2>
       {location && (
-        <p>
+        <p className={classes.location}>
           <MdLocationPin />
           <span>{location}</span>
         </p>
       )}
-      <div>
+      <div className={classes.stats}>
         <div>
           <p>Seguidores:</p>
-          <p>{followers}</p>
+          <p className={classes.number}>{followers}</p>
         </div>
         <div>
           <p>Seguindo:</p>
-          <p>{following}</p>
+          <p className={classes.number}>{following}</p>
         </div>
         <Link to={'/repos/' + login}>Ver melhores projetos</Link>
       </div>
