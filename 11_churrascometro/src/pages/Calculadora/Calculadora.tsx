16d3d32632cf1d02ed09be2d9e6998b7cad0ddd6
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-29 21:54:41 -0300

    Cria pagina de churrasco adiciona o estilos e faz a validacoes

--- 11_churrascometro/src/pages/Calculadora/Calculadora.tsx
+++ 11_churrascometro/src/pages/Calculadora/Calculadora.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import bgImage from '../../assets/bg-1.jpg';
+import CalculadoraChurrasco from '../../components/CalculadoraChurrasco/CalculadoraChurrasco';
 
 const Calculadora = () => {
-  return <div>Cal</div>;
+  return (
+    <div
+      className="page-container"
+      style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover' }}
+    >
+      <h1>Calculadora de Churraco</h1>
+      <CalculadoraChurrasco />
+    </div>
+  );
 };
 
 export default Calculadora;
