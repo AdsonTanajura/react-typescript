43375014157b1314b9cb4c45010a7dc5a834ea55
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-30 16:48:23 -0300

    resgatando repositorio do usuario

--- 12_github_finder/src/routes/Repos/Repos.tsx
+++ 12_github_finder/src/routes/Repos/Repos.tsx
@@ -1,11 +1,30 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { RepoPros } from '../../types/repo';
 
 import classes from './Repos.module.css ';
 import BackBtn from '../../components/BackBtn/BackBtn';
+import Loader from '../../components/Loader/Loader';
 
 const Repos = () => {
   const { username } = useParams();
+  const [repo, setRepo] = useState<RepoPros[] | [] | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const loadRepos = async (username: string) => {
+      setIsLoading(true);
+
+      const res = await fetch(`https://api.github.com/users/${username}/repos`);
+
+      const data = await res.json();
+
+      setIsLoading(false);
+    };
+
+    loadRepos(username);
+  }, []);
+
   return (
     <div>
       <BackBtn />
