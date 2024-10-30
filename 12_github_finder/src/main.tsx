2a772a71841c64fe5bd0d15c3284cfb45f33012b
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-30 16:39:09 -0300

    Criando a pagina de repositorios

--- 12_github_finder/src/main.tsx
+++ 12_github_finder/src/main.tsx
@@ -6,6 +6,7 @@ import App from './App.tsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Home from './routes/Home/Home.tsx';
+import Repos from './routes/Repos/Repos.tsx';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: '/repos/:username',
+        element: <Repos />,
       },
     ],
   },
