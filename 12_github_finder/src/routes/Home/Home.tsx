3f49d1902f0b925e5daae092ee9fa72fa8fe9f09
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-30 16:02:34 -0300

    Componente de erro

--- 12_github_finder/src/routes/Home/Home.tsx
+++ 12_github_finder/src/routes/Home/Home.tsx
@@ -3,14 +3,24 @@ import { useState } from 'react';
 import { userProps } from '../../types/user';
 
 import User from '../../components/User/User';
+import Error from '../../components/Error/Error';
 const Home = () => {
   const [user, setUser] = useState<userProps | null>(null);
+  const [error, setError] = useState(false);
 
   const loaderUser = async (userName: string) => {
+    setError(false);
+    setUser(null);
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
     const { avatar_url, login, followers, following, location }: userProps =
       data;
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
+
     const userData: userProps = {
       avatar_url,
       followers,
@@ -26,6 +36,7 @@ const Home = () => {
     <div>
       <Search loadUser={loaderUser} />
       {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
